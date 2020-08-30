062c9970ced2dfeb5a3a51a34b677fc72e7c7786
sterenziani <sterenziani@itba.edu.ar>
2020-08-29 21:33:05 -0300

    Added Rank Selection

--- src/main/java/PopulationFilter.java
+++ src/main/java/PopulationFilter.java
@@ -7,7 +7,7 @@ public class PopulationFilter {
 	{
 		population.sort((Character p1, Character p2) -> Double.compare(p2.getPerformance(), p1.getPerformance())); // Highest performance to lowest
 		List<Character> list = new ArrayList<>();
-		for(int i=0; i < selected && i < population.size(); i++)
+		for(int i=0; i < selected; i++)
 		{
 			list.add(population.get(i));
 		}
@@ -17,15 +17,15 @@ public class PopulationFilter {
 	
 	public static List<Character> rouletteSelection(List<Character> population, int selected)
 	{
-		return rouletteUniversal(population, selected, false);
+		return performanceRoulette(population, selected, false);
 	}
 
 	public static List<Character> universalSelection(List<Character> population, int selected)
 	{
-		return rouletteUniversal(population, selected, true);
+		return performanceRoulette(population, selected, true);
 	}
 
-	private static List<Character> rouletteUniversal(List<Character> population, int selected, boolean universal)
+	private static List<Character> performanceRoulette(List<Character> population, int selected, boolean universal)
 	{
 		// Calculate relative performance (p) and acum relative performances (q)
 		double totalPerformance = 0;
@@ -57,6 +57,49 @@ public class PopulationFilter {
 			else
 				r[j] = Math.random();		// Roulette
 			
+			if(r[j] <= q[0])
+				list.add(population.get(0));
+			else for(int i=1; i < q.length; i++)
+			{
+				if(q[i-1] < r[j] && r[j] <= q[i])
+				{
+					list.add(population.get(i));
+					i = q.length; // end i loop
+				}
+			}
+		}
+		return list;
+	}
+	
+	public static List<Character> rankSelection(List<Character> population, int selected)
+	{
+		population.sort((Character p1, Character p2) -> Double.compare(p2.getPerformance(), p1.getPerformance())); // Highest performance to lowest
+		double f[] = new double[population.size()];
+		for(int i=0; i < selected; i++)
+		{
+			// Since they're ordered from highest to lowest, i equals the rank
+			f[i] = 1.0 - i/selected;
+		}
+		
+		// From now on, it's like roulette but using f instead of performance
+		double totalPerformance = 0;
+		double[] p = new double[population.size()];
+		double[] q = new double[population.size()];
+		for(int i=0; i < population.size(); i++)
+			totalPerformance += f[i];
+		for(int i=0; i < population.size(); i++)
+		{
+			p[i] = f[i] / totalPerformance;
+			q[i] = 0;
+			for(int j=0; j <= i; j++)
+				q[i] += p[i];
+		}
+		
+		double[] r = new double[selected];
+		List<Character> list = new ArrayList<>();
+		for(int j=0; j < selected; j++)
+		{
+			r[j] = Math.random();
 			if(r[j] <= q[0])
 				list.add(population.get(0));
 			else for(int i=1; i < q.length; i++)
